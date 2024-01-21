f27a109c53ece70f592bd179cf442785b127847e
Colin Behan <154695748+B3han@users.noreply.github.com>
2024-01-21 11:27:33 -0500

    Add files via upload
    For loop practice

---
+++ for_tutorial.py
@@ -0,0 +1,33 @@
+# expenses = [1200, 1300, 2100, 1700, 2300]
+#
+# for i, expense in enumerate(expenses):
+#     if expense>2000:
+#         print(f"Month {i+1} has expense > 2000")
+#         break
+#
+
+# locations = ["sofa", "garage", "chair", "table", "closet"]
+#
+# key_location = "chair"
+#
+# for location in locations:
+#     if location == key_location:
+#         print("Key is found at ", location)
+#         break
+#     else:
+#         print("Key is not found at ", location)
+#
+
+# for i in range(1,11):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+#
+
+n = 0
+total = 0
+
+while n <= 10:
+    print(n)
+    n += 1
+
